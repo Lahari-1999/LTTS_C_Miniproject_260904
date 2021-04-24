7c2279600bc19ce69cc620158aa19b30125d4fdf
Lahari-1999 <laharilucky99@gmail.com>
2021-04-24 16:07:33 +0530

    added 3_Implementation

--- 3_Implementation/src/choose_module.c
+++ 3_Implementation/src/choose_module.c
@@ -1,24 +1,29 @@
-#include "calendar.h" //header file 
+#include "calendar.h"
 
-int choose_module(void)//user options selection module
+int choose_module(void)
 {
-	int option; //variable to store user input
+	int option; 
 	printf("\t\tChoose Module :\n\n");
 	printf("\t\t1) YEAR INFO\n");
 	printf("\t\t2) DAY  INFO\n");
 	printf("\t\t3) YEARLY CALENDAR\n");
-	printf("\t\t4) MONTHLY CALENDAR\n\n");
+	printf("\t\t4) MONTHLY CALENDER\n\n");
+	printf("\t\t5) EXIT\n");
 	printf("\t\tWating for user input ... |  ");
 	scanf("%d",&option);
 	if(option>=1 && option<=4 )
 	{
-		return option; //function returns user choice
+		return option;
 	}
-	else  //error handling
+	else if(option==5)
 	{
-		system("cls");//clears termnial screen
+		exit(0);
+	}
+	else
+	{
+		system("cls");
 		printf("\n\t\tINVALID ENTRY || PLEASE TRY AGAIN\n\n");		
-		choose_module();//loopingfunction - if wrong module selected
+		choose_module();
 	}
 	
 }
